9bf4d584a617bd247367fc44adb0d94420b054a8
Yeonju Park <yeonjupark95@gmail.com>
2022-03-08 21:30:22 -0800

    added a placeholder for image in products table

--- db/seedData.js
+++ db/seedData.js
@@ -51,7 +51,8 @@ async function createTables() {
           id SERIAL PRIMARY KEY,
           name VARCHAR(255) NOT NULL,
           description TEXT NOT NULL,
-          price DECIMAL
+          price DECIMAL,
+          photo VARCHAR(2048)
         );
 
         CREATE TABLE products_reviews (
