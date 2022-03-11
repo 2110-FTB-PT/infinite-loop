4efeb44bd9e2b55030a642273be4cc685d022990
Lindsay Naki <lindsnakamura@gmail.com>
2022-03-10 23:37:23 -0800

    added products_orders database adapters

--- db/models/products_orders.js
+++ db/models/products_orders.js
@@ -1,6 +1,85 @@
 const client = require('../client');
 
+const getProductOrdersById = async (id) => {
+    try {
+        const { rows: [product_order] } = await client.query(`
+            SELECT * FROM products_orders
+            WHERE id=$1
+        `, [id])
+
+        return product_order;
+    } catch(error) {
+        throw error; 
+    }
+}
+
+const addProductToOrder = async ({ orderId, productId, quantity }) => {
+    try {
+        const { rows: [order] } = await client.query(`
+            INSERT INTO products_orders ("orderId", "productId", quantity)
+            VALUES ($1, $2, $3, $4)
+            RETURNING *; 
+        `, [orderId, productId, quantity]);
+
+        return order; 
+    } catch(error) {
+        throw error;
+    }
+}
+
+const updateProductOrder = async ({ id, ...orderFields}) => {
+    const setString = Object.keys(orderFields).map((key, index) => `"${key}"=$${index +1}`).join("");
+
+    if (setString.length === 0) {
+        return;
+    }
+
+    try {
+        const { rows: [product_order] } = await client.query(`
+            UPDATE products_orders 
+            SET ${setString}
+            WHERE id=${id}
+            RETURNING *;
+        `, Object.values(orderFields))
+
+        return product_order;
+    } catch(error) {
+        throw error; 
+    }
+}
+
+const getProductsOrdersByOrder = async ({ id }) => {
+    try {
+        const { rows: product_order } = await client.query(`
+            SELECT * FROM products_orders
+            WHERE "orderId"=$1;
+        `, [id]);
+
+        return product_order;
+    } catch(error) {
+        throw error;
+    }
+}
+
+const deleteProductOrder = async (id) => {
+    try {
+        const { rows: [product_order] } = await client.query(`
+            DELETE FROM products_orders
+            WHERE id=$1
+            RETURNING *
+        `, [id]);
+
+        return product_order;
+    } catch(error) {
+        throw error;
+    }
+}
+
 
 module.exports = {
-    // add your database adapter fns here
+    getProductOrdersById,
+    addProductToOrder,
+    updateProductOrder,
+    getProductsOrdersByOrder,
+    deleteProductOrder
   };
