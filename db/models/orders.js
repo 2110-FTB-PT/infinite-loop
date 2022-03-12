15274308e6df0378a011a011e3694a7b143c6fe8
Yeonju Park <yeonjupark95@gmail.com>
2022-03-11 21:43:19 -0800

    merge conflict from dev

--- db/models/orders.js
+++ db/models/orders.js
@@ -3,33 +3,6 @@ const client = require("../client");
 
 const addProductsToOrders = async (orders) => {
   try {
-<<<<<<< HEAD
-      const orderIdArray = orders.map((order) => {
-          return order.id
-      })
-      const { rows: products_orders } = await client.query(`
-          SELECT products FROM products
-          JOIN products_orders ON products_orders."productId"=products.id
-          WHERE products_orders."orderId" IN (${orderIdArray});
-      `)
-
-      orders.forEach((order) => {
-        order.products = products.filter((product) => {
-          return product.orderId === order.id
-        })
-      })
-
-      return orders;
-  } catch(error) {
-      throw error;
-  }
-}
-// adding products to each order
-// first find each order id 
-// select all products from products table on the condition that the product id is equal to the "productId" from the products_orders
-// on another condition in which the orderid from products_tables is also in the order id 
-// after grabbing those products that are in the specified order id, we add those products to the individual order object 
-=======
     const orderIdArray = orders.map((order) => {
       return order.id;
     });
@@ -55,7 +28,6 @@ const addProductsToOrders = async (orders) => {
 // select all products from products table on the condition that the product id is equal to the "productId" from the products_orders
 // on another condition in which the orderid from products_tables is also in the order id
 // after grabbing those products that are in the specified order id, we add those products to the individual order object
->>>>>>> bcf4d0d2b8a47817e4fab0adca6f628d122a6069
 // we map through each of ther orders and add the products as a key value pair to each order on the condition that the order.id is equal to product.orderid which comes from the joining of tables
 
 const getAllOrders = async () => {
@@ -90,13 +62,7 @@ const getOrderById = async (id) => {
 
 const getOrdersByUser = async ({ username }) => {
   try {
-<<<<<<< HEAD
-    const {
-      rows: orders,
-    } = await client.query(
-=======
     const { rows: orders } = await client.query(
->>>>>>> bcf4d0d2b8a47817e4fab0adca6f628d122a6069
       `
             SELECT orders.*, users.username AS "customerName"
             FROM orders
@@ -113,13 +79,7 @@ const getOrdersByUser = async ({ username }) => {
 
 const getOrdersByStatus = async ({ status }) => {
   try {
-<<<<<<< HEAD
-    const {
-      rows: orders,
-    } = await client.query(
-=======
     const { rows: orders } = await client.query(
->>>>>>> bcf4d0d2b8a47817e4fab0adca6f628d122a6069
       `
             SELECT * FROM orders
             WHERE status = $1
@@ -132,47 +92,17 @@ const getOrdersByStatus = async ({ status }) => {
   }
 };
 
-<<<<<<< HEAD
-const createUserOrder = async ({ userId, fullName, address, status }) => {
-  try {
-    const {
-      rows: [order],
-    } = await client.query(
-        `
-            INSERT INTO products (userId, fullName, address, status)
-            VALUES ($1, $2, $3)
-            RETURNING *;
-        `,
-      [userId, fullName, address, status]
-    );
-    return order;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const createGuestOrder = async ({ fullName, address, status }) => {
-=======
 const createOrder = async ({ userId, email, address, status }) => {
->>>>>>> bcf4d0d2b8a47817e4fab0adca6f628d122a6069
   try {
     const {
       rows: [order],
     } = await client.query(
         `
-<<<<<<< HEAD
-            INSERT INTO products (fullName, address, status)
-            VALUES ($1, $2, $3)
-            RETURNING *;
-        `,
-      [fullName, address, status]
-=======
             INSERT INTO products (userId, email, address, status)
             VALUES ($1, $2, $3, $4)
             RETURNING *;
         `,
       [userId, email, address, status]
->>>>>>> bcf4d0d2b8a47817e4fab0adca6f628d122a6069
     );
     return order;
   } catch (error) {
@@ -230,12 +160,7 @@ module.exports = {
   getOrderById,
   getOrdersByUser,
   getOrdersByStatus,
-<<<<<<< HEAD
-  createUserOrder,
-  createGuestOrder,
-=======
   createOrder,
->>>>>>> bcf4d0d2b8a47817e4fab0adca6f628d122a6069
   updateOrder,
   deleteOrder,
 };
