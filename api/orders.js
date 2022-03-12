96fd1bb24ef584abd50a0644cfeb9e49dd5a455c
Yeonju Park <yeonjupark95@gmail.com>
2022-03-11 20:30:58 -0800

    added require to db models index

--- api/orders.js
+++ api/orders.js
@@ -1,4 +1,14 @@
 const ordersRouter = require("express").Router();
 //insert query requests here//
 
+const {
+  addProductsToOrders,
+  getAllOrders,
+  getOrderById,
+  getOrdersByUser,
+  getOrdersByStatus,
+  createOrder,
+  updateOrder,
+  deleteOrder,
+} = require("../db/orders");
 module.exports = ordersRouter;
