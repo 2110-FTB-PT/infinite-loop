f5fb4e0428d75daef3b9b17f26293105fe343429
Yeonju Park <yeonjupark95@gmail.com>
2022-03-12 17:43:45 -0800

    CRUD added for api orders

--- api/orders.js
+++ api/orders.js
@@ -11,39 +11,127 @@ const {
 // TODO: const { requireAdmin } = require("./utils");
 
 // TODO: require admin
-ordersRouter.get("/", async (req, res, next) => {
+ordersRouter.get("/all", async (req, res, next) => {
   try {
     const orders = await getAllOrders();
     res.send(orders);
   } catch (error) {
-    next(error);
+    next({
+      name: "CannotFetchOrders",
+      message: "Cannot get all orders",
+    });
   }
 });
 
+// TODO: require user
 ordersRouter.get("/:orderId", async (req, res, next) => {
-  const { orderId } = req.params;
   try {
+    const { orderId } = req.params;
     const order = await getOrderById(orderId);
     res.send(order);
   } catch (error) {
-    next(error);
+    next({
+      name: "OrderDoesNotExist",
+      message: "There are no orders matching orderId",
+    });
   }
 });
 
+// TODO: require admin
 ordersRouter.get("/:username", async (req, res, next) => {
-  const { username } = req.params;
   try {
-    const orders = await getOrderByUser(username);
+    const { username } = req.params;
+    const orders = await getOrdersByUser(username);
     res.send(orders);
   } catch (error) {
+    next({
+      name: "OrderDoesNotExist",
+      message: "There are no orders matching username",
+    });
+  }
+});
+
+// TODO: require admin
+ordersRouter.get("/:status", async (req, res, next) => {
+  try {
+    const { status } = req.params;
+    const orders = await getOrdersByStatus(status);
+    res.send(orders);
+  } catch (error) {
+    next({
+      name: "OrderDoesNotExist",
+      message: "There are no orders matching status",
+    });
+  }
+});
+
+// TODO: require user
+ordersRouter.post("/:userId", async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const { email, address, status } = req.body;
+    const newOrder = await createOrder({
+      id: userId,
+      email,
+      address,
+      status,
+    });
+    res.send(newOrder);
+  } catch (error) {
+    next({
+      name: "OrderDoesNotExist",
+      message: "There are no orders matching userId",
+    });
+  }
+});
+
+// TODO: require user
+ordersRouter.patch("/:orderId", async (req, res, next) => {
+  const { orderId } = req.params;
+  const { email, address, status } = req.body;
+  try {
+    const order = await getOrderById(orderId);
+    if (order.userId === req.user.id) {
+      const updatedOrder = await updateOrder({
+        id: orderId,
+        email,
+        address,
+        status,
+      });
+      res.send(updatedOrder);
+      return;
+    } else {
+      next({
+        name: "updateOrderCredentialError",
+        message: "You must be the owner of this order to update the order",
+      });
+    }
+  } catch (error) {
     next(error);
   }
 });
 
-ordersRouter.post("/", async (req, res, next) => {
+// TODO: require user
+ordersRouter.delete("/:orderId", async (req, res, next) => {
+  const { orderId } = req.params;
+  const { email, address, status } = req.body;
   try {
+    const order = await getOrderById(orderId);
+    if (order.userId === req.user.id) {
+      const updatedOrder = await updateOrder({
+        id: orderId,
+        email,
+        address,
+        status,
+      });
+      res.send(updatedOrder);
+      return;
+    }
   } catch (error) {
-    next(error);
+    next({
+      name: "deleteOrderCredentialError",
+      message: "You must be the owner of this order to delete the order",
+    });
   }
 });
 
