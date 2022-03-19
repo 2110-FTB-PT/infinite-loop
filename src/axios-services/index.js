716713b57388c09dd946c3f503931f083e2f44f0
Lindsay Naki <lindsnakamura@gmail.com>
2022-03-18 22:09:43 -0700

    added fetchAllProducts to index

--- src/axios-services/index.js
+++ src/axios-services/index.js
@@ -50,3 +50,13 @@ export const createPendingOrder = async (email, address) => {
     console.error(error);
   }
 };
+
+export const fetchAllProducts = async () => {
+  try {
+    const { data: products } = await axios.get(`${BASE_URL}/products`);
+    console.log('all products: ', products);
+    return products;
+  } catch(error) {
+    throw error;
+  }
+}
