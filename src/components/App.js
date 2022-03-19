188aba7261551813e900078da8cc36c87d34f1c7
151177 <36523610+151177@users.noreply.github.com>
2022-03-19 15:03:54 -0600

    added tokens to login

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,66 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-
-import AccountForm from "./AccountForm";
+import { getUser } from "../db";
+import LoginForm from "./LoginForm";
+// import RegisterForm from "./RegisterForm";
 import Navigation from "./Navigation";
 import Home from "./Home";
 import Footer from "./Footer";
 
-// getAPIHealth is defined in our axios-services directory index.js
-// you can think of that directory as a collection of api adapters
-// where each adapter fetches specific info from our express server's /api route
-import { getAPIHealth } from "../axios-services";
+// // getAPIHealth is defined in our axios-services directory index.js
+// // you can think of that directory as a collection of api adapters
+// // where each adapter fetches specific info from our express server's /api route
+// import { getAPIHealth } from "../axios-services";
 
 const App = () => {
-  const [APIHealth, setAPIHealth] = useState("");
+  //   const [APIHealth, setAPIHealth] = useState("");
+
+  //   useEffect(() => {
+  //     // follow this pattern inside your useEffect calls:
+  //     // first, create an async function that will wrap your axios service adapter
+  //     // invoke the adapter, await the response, and set the data
+  //     const getAPIStatus = async () => {
+  //       const { healthy } = await getAPIHealth();
+  //       setAPIHealth(healthy ? "api is up! :D" : "api is down :/");
+  //     };
+
+  //     // second, after you've defined your getter above
+  //     // invoke it immediately after its declaration, inside the useEffect callback
+  //     getAPIStatus();
+  //   }, []);
+
+  const [token, setToken] = useState("");
+  const [user, setUser] = useState({});
+
+  const handleUser = async () => {
+    if (token) {
+      const userObject = await getUser(token);
+      setUser(userObject);
+    } else {
+      setUser({});
+    }
+  };
 
   useEffect(() => {
-    // follow this pattern inside your useEffect calls:
-    // first, create an async function that will wrap your axios service adapter
-    // invoke the adapter, await the response, and set the data
-    const getAPIStatus = async () => {
-      const { healthy } = await getAPIHealth();
-      setAPIHealth(healthy ? "api is up! :D" : "api is down :/");
-    };
+    handleUser();
+  }, [token]);
 
-    // second, after you've defined your getter above
-    // invoke it immediately after its declaration, inside the useEffect callback
-    getAPIStatus();
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
+    }
   }, []);
 
   return (
     <div className='app-container'>
-      <Navigation />
+      <Navigation token={token} setToken={setToken} />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<AccountForm />} />
+        <Route path='/login' element={<LoginForm setToken={setToken} />} />
+        {/* <Route
+          path='/register'
+          element={<RegisterForm token={token} setToken={setToken} />}
+        /> */}
       </Routes>
       <Footer />
     </div>
---
+++ src/components/RegisterForm.js
@@ -0,0 +1,59 @@
+// import React, { useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import "../style/AccountForm.css";
+
+// const RegisterForm = ({ setToken }) => {
+//   const [username, setUserName] = useState("");
+//   const [password, setPassword] = useState("");
+//   const navigate = useNavigate();
+
+//   //   const handleSubmit = async (event) => {
+//   //     event.preventDefault();
+//   //     const token = await login(username.toLowerCase(), password);
+//   //     localStorage.setItem("token", token);
+//   //     setToken(token);
+//   //     navigate("/");
+//   //   };
+
+//   return (
+//     <div className='account-form-container'>
+//       <form className='account-form-content-container' onSubmit={handleSubmit}>
+//         <div className='account-form-header'>Register</div>
+//         <div className='account-form-content'>
+//           <label className='account-form-label'>Username:</label>
+//           <input
+//             className='account-form-input'
+//             required
+//             value={username}
+//             onChange={(event) => {
+//               setUserName(event.target.value);
+//             }}
+//           />
+//         </div>
+
+//         <div className='account-form-content'>
+//           <label className='account-form-label'>Password:</label>
+//           <input
+//             className='account-form-input'
+//             required
+//             type='password'
+//             value={password}
+//             onChange={(event) => {
+//               setPassword(event.target.value);
+//             }}
+//           />
+//         </div>
+//         <button className='account-form-button'>Submit</button>
+//         <div className='account-form-additional'>
+//           Already a member?
+//           <Link className='account-form-additional-path' to={"/login"}>
+//             {" "}
+//             Sign In
+//           </Link>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default RegisterForm;
