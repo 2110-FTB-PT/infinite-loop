280a893fd97c9b7c8ac33824f16068eaf3d29133
Lindsay Naki <lindsnakamura@gmail.com>
2022-03-19 20:17:33 -0700

    rendered products on shop all collection

--- src/components/ShopAll.js
+++ src/components/ShopAll.js
@@ -1,14 +1,32 @@
 import React from "react";
+import { useState, useEffect } from 'react';
+import { fetchAllProducts } from '../axios-services/index'
 
 const ShopAll = () => {
+    const [ products, setProducts ] = useState([])
 
     const handleProducts = async () => { 
-        
+        const fetchedProducts = await fetchAllProducts();
+        setProducts(fetchedProducts);
     } 
+
+    useEffect(() => {
+        handleProducts();
+    }, []);
 
     return ( 
         <div>
             <h1>All Products</h1>
+            {products.map((product) => {
+                const { name, price, photo } = product
+                return (
+                    <div>
+                        <p>{name}</p>
+                        <p>{photo}</p>
+                        <p>{price}</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
