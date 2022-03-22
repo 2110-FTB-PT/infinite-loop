48553721e2737dbcc810a4ab9699081e69ac2255
Yeonju Park <yeonjupark95@gmail.com>
2022-03-21 22:32:42 -0700

    made products storage inside SingleCartProduct

--- src/components/Order/SingleCartProduct.js
+++ src/components/Order/SingleCartProduct.js
@@ -13,20 +13,21 @@ const SingleCartProduct = ({
   console.log("singlecartProduct.js", cart);
   const [products, setProducts] = useState([]);
   const [productQuantity, setProductQuantity] = useState(1);
-  const [productsStorage, setProductsStorage] = useState({});
 
   const handleCartProducts = async () => {
     // based on the orderId, grabbing all the products in an array!
     const orderCartProducts = await fetchProductOrderById(cart.id);
     setCartProducts(orderCartProducts);
     console.log("orderCartProduct", orderCartProducts);
+    let productsStorage = [];
     for (let i = 0; i < orderCartProducts.length; i++) {
       const orderProductId = orderCartProducts[i].productId;
-      // setProductQuantity(orderCartProducts[i].quantity);
       const fetchedProduct = await fetchProductById(orderProductId);
+      productsStorage[i] = fetchedProduct;
       console.log("fetchedProduct", fetchedProduct);
-      setProducts([fetchedProduct]); // [{}, {}, {}]
     }
+    console.log("productsStorage", productsStorage);
+    setProducts(productsStorage); // [{}, {}, {}]
   };
 
   useEffect(() => {
