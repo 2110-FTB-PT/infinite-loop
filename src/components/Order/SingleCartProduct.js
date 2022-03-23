3e688e659720eb877cc4fb35fbcf495a0c324b70
Yeonju Park <yeonjupark95@gmail.com>
2022-03-22 20:53:36 -0700

    removed console logs

--- src/components/Order/SingleCartProduct.js
+++ src/components/Order/SingleCartProduct.js
@@ -2,26 +2,22 @@ import React, { useState, useEffect } from "react";
 import { fetchProductOrderById, fetchProductById } from "../../axios-services";
 
 const SingleCartProduct = ({ cart }) => {
-  console.log("singlecartProduct.js", cart);
   const [products, setProducts] = useState([]);
   const [cartProducts, setCartProducts] = useState([]);
 
   const handleCartProducts = async () => {
-    // based on the orderId, grabbing all the products in an array!
+    // based on the orderId, grabbing all the products in an array
     const cartProductOrder = await fetchProductOrderById(cart.id);
     setCartProducts(cartProductOrder);
-    console.log("cartProductOrder", cartProductOrder);
-    let productsInfo = [];
 
+    let productsInfo = [];
     // for each product in Product Order, need to bring name and price by Product Id from Products Table and quantity from Product Order Table
     for (let i = 0; i < cartProductOrder.length; i++) {
       const productId = cartProductOrder[i].productId;
       const cartProduct = await fetchProductById(productId);
       const productQty = cartProductOrder[i].quantity;
       productsInfo[i] = { cartProduct, productQty };
-      console.log("cartProduct", cartProduct);
     }
-    console.log("productsInfo", productsInfo);
     setProducts(productsInfo);
   };
 
