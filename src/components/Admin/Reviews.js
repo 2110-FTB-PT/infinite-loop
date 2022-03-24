434102b00367074b735afa4733610ae5de020cc2
Lindsay Naki <lindsnakamura@gmail.com>
2022-03-24 03:45:28 -0700

    rendered product on reviews on admin

--- src/components/Admin/Reviews.js
+++ src/components/Admin/Reviews.js
@@ -1,34 +1,54 @@
 import React from "react";
+import { useState, useEffect  } from "react";
 import { Link } from "react-router-dom";
 import { deleteReview, fetchReviews } from "../../axios-services/index";
 
-const Reviews = ({ user, token, reviews, setReviews }) => {
+const Reviews = () => {
+    const [reviews, setReviews] = useState([])
 
-    const handleDelete = async (id) => {
-        try {
-            await deleteReview(id, token)
-            const newReviews = reviews.filter((routine) => {
-                return routine.id !== id;
-            });
+    // const handleDelete = async (id) => {
+    //     try {
+    //         await deleteReview(id, token)
+    //         const newReviews = reviews.filter((routine) => {
+    //             return routine.id !== id;
+    //         });
 
-            const allReviews = await fetchReviews();
-            console.log('allReviews')
-            setReviews(allReviews);
-        }   catch (error) {
-            console.error(error);
-        }
+    //         const allReviews = await fetchReviews();
+    //         console.log('allReviews')
+    //         setReviews(allReviews);
+    //     }   catch (error) {
+    //         console.error(error);
+    //     }
+    // }
+
+    const handleReviews = async () => {
+        const allReviews = await fetchReviews();
+        console.log('all react reviews: ', allReviews)
+        setReviews(allReviews);
     }
+
+    useEffect(() => {
+        handleReviews();
+    }, []);
+
     return (
         <div>
             <Link to="/admin"><h1>Back to Admin Dashboard</h1></Link>
             <h1>Reviews</h1>
                         {reviews.map((review) => {
+                            const { id, description, rating, products } = review; 
                             return (
                             <div key={ review.id }>
-                                <div> productId: {review.productId} </div>
-                                <div> Description: {review.description} </div>
-                                <div> Rating: {review.rating} </div>
-                                {(user?.id === review.id) && <button onClick={() => handleDelete(review.id)}> Delete </button> }
+                                <p> Description: {description} </p>
+                                <p> Rating: {rating} </p>
+                                {products.map((product) => {
+                                    const { name } = product
+                                    return (
+                                        <div>
+                                            <p>{name}</p>
+                                        </div>
+                                    )
+                                })}
                             </div>
                             )
                         })}
