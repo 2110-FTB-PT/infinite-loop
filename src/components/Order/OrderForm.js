94f8ec4169ebae7f9ed924fde24d8f3e38ee53f3
Yeonju Park <yeonjupark95@gmail.com>
2022-03-25 23:45:22 -0700

    i need to see something

--- src/components/Order/OrderForm.js
+++ src/components/Order/OrderForm.js
@@ -3,13 +3,21 @@ import "../../style/Cart.css";
 import { updateOrder } from "../../axios-services";
 
 const OrderForm = ({ cart }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [address, setAddress] = useState("");
 
   const handleCreateOrder = async (event) => {
     try {
       event.preventDefault();
-      const newUpdatedOrder = await updateOrder(cart.id, email, address);
+      const newUpdatedOrder = await updateOrder(
+        cart.id,
+        firstName,
+        lastName,
+        email,
+        address
+      );
     } catch (error) {
       console.error(error);
     }
@@ -18,6 +26,18 @@ const OrderForm = ({ cart }) => {
     <div className="order-form">
       <div> Checkout </div>
       <form onSubmit={handleCreateOrder}>
+        <input
+          type="text"
+          placeholder="first name*"
+          onChange={(event) => setFirstName(event.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="last name*"
+          onChange={(event) => setLastName(event.target.value)}
+          required
+        />
         <input
           type="text"
           placeholder="email*"
