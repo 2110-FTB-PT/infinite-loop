167a7583f08d3898965f09e9439207a1e28e6e41
Lindsay Naki <lindsnakamura@gmail.com>
2022-03-28 19:29:32 -0700

    rendered delete button reviews if author of review

--- src/components/ReviewsByProduct.js
+++ src/components/ReviewsByProduct.js
@@ -6,10 +6,12 @@ import "../style/ReviewsByProduct.css";
 
 const ReviewsByProduct = ({ id, token, user }) => {
   const [productReview, setProductReview] = useState([]);
+  const { id: userObjectId } = user
 
   const handleReviewsByProduct = async () => {
     try {
       const reviews = await reviewsByProduct(id);
+      console.log('reviews: ', reviews)
       setProductReview(reviews);
     } catch (error) {
       console.error(error);
@@ -47,19 +49,23 @@ const ReviewsByProduct = ({ id, token, user }) => {
         <div className='customer-review-header'>Customer Reviews</div>
         {productReview ? (
           productReview.map((review) => {
+            const { userId, description, rating, user } = review;
+            console.log('reviewuserId', userId)
+            console.log('user object id', user.id)
             return (
               <div className='review-posted-content' key={review.id}>
                 <div className='review-posted-description'>
                   {" "}
-                  {review.description}
+                  {description}
                 </div>
                 <div className='review-posted-rating'>
-                  Rated: {review.rating}
+                  Rated: {rating}
                 </div>{" "}
-                <div className='review-posted-by-user'>
-                  Reviewed by {review.userId}
-                </div>
-                {user?.id === review.userId && (
+                {user.username === 'guest' 
+                  ? <div className='review-posted-by-user'>Reviewed by anonymous</div> 
+                  : <div className='review-posted-by-user'>Reviewed by {user.username}
+                </div>}
+                {userObjectId === review?.userId && (
                   <button
                     className='review-posted-button'
                     onClick={() => handleDelete(review.id)}
