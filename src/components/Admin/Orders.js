edd527fb55d9d5811f823c163bd6773cc54a7550
151177 <36523610+151177@users.noreply.github.com>
2022-03-30 19:13:24 -0700

    designed initial admin pages: dashboard, products, orders, accounts, and reviews

--- src/components/Admin/Orders.js
+++ src/components/Admin/Orders.js
@@ -2,53 +2,69 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { fetchAllOrders } from "../../axios-services";
-import { FaRegEdit } from 'react-icons/fa'
-import "../../style/Orders.css"
+import { FaRegEdit } from "react-icons/fa";
+import "../../style/Orders.css";
+import "../../style/Admin.css";
 
 const Orders = () => {
-    const navigate = useNavigate();
-    const [ orders, setOrders ] = useState([])
+  const navigate = useNavigate();
+  const [orders, setOrders] = useState([]);
 
-    const handleOrders = async () => {
-        const allOrders = await fetchAllOrders();
-        setOrders(allOrders)
-    }
+  const handleOrders = async () => {
+    const allOrders = await fetchAllOrders();
+    setOrders(allOrders);
+  };
 
-    useEffect(() => {
-        handleOrders()
-    }, []);
+  useEffect(() => {
+    handleOrders();
+  }, []);
 
-    return (
-        <div>
-            <Link to="/admin"><h1>Back to Admin Dashboard</h1></Link>
-            <h1>Total Orders: {orders.length}</h1>
-            <h1>All Orders</h1>
-            <div className="table-wrapper">
-                <table className="orders-table">
-                    <tr className="table-headers">
-                        <th>Order #</th>
-                        <th>Customer</th>
-                        <th>Status</th>
-                        <th><FaRegEdit /></th>
-                    </tr>
-                    {orders.map((order) => {
-                        const { id, first_name, last_name, currentStatus } = order;
-                        return (
-                            <tr>
-                                <td>{id}</td>
-                                {first_name && last_name ? <td>{first_name} {last_name}</td> : <td> Guest </td>}
-                                <td>{currentStatus}</td>
-                                <td><FaRegEdit 
-                                    role="button"
-                                    onClick={() => navigate(`/admin/orders/${id}`)}
-                                /></td>
-                            </tr>      
-                        )
-                    })}
-                </table>
-            </div>
+  return (
+    <div className='general-dashboard-container'>
+      <div className='general-dashboard-content'>
+        <Link to='/admin' className='general-dashboard-back-link'>
+          Back to Dashboard
+        </Link>
+        <div className='general-dashboard-header'>Orders</div>
+        <div className='general-dashboard-subheader'>
+          Total Orders: {orders.length}
         </div>
-    )
-}
+
+        <div className='table-wrapper'>
+          <table className='orders-table'>
+            <tr className='table-headers'>
+              <th>Order #</th>
+              <th>Customer</th>
+              <th>Status</th>
+              <th>Edit</th>
+            </tr>
+            {orders.map((order) => {
+              const { id, first_name, last_name, currentStatus } = order;
+              return (
+                <tr>
+                  <td>{id}</td>
+                  {first_name && last_name ? (
+                    <td>
+                      {first_name} {last_name}
+                    </td>
+                  ) : (
+                    <td> Guest </td>
+                  )}
+                  <td>{currentStatus}</td>
+                  <td>
+                    <FaRegEdit
+                      role='button'
+                      onClick={() => navigate(`/admin/orders/${id}`)}
+                    />
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Orders;
